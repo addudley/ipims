d7279bb3e14fc74c81e1c02ffcde8945eebab765
Andy <andy@nau.edu>
2015-10-13 18:38:23 -0700

    Test Cases added for patients, search, and appointments

--- src/appointments/tests.py
+++ src/appointments/tests.py
@@ -1,1 +1,18 @@
 from django.test import TestCase
+from .models import Appointment
+
+class ScheduleAppointmentViewTestCase(TestCase):
+# Verify Schedule Appointment view loads successfully
+	def test_schedule_appointment_view(self):
+		resp = self.client.get('/appointments/add/')
+		self.assertEqual(resp.status_code, 200)
+
+class AppointmentDetailsViewTestCase(TestCase):
+# Verify Schedule Appointment view loads successfully
+	fixtures = ['appointments_testdata.json', 'patients_testdata.json'] # Load test data (appointments, Patient, User [doctors])
+	def test_schedule_appointment(self):
+		resp = self.client.get('/appointments/1/')
+		appointment = resp.context['appointment']
+		self.assertEqual(appointment.id, 1)
+		self.assertEqual(resp.status_code, 200)
+
--- src/patients/tests.py
+++ src/patients/tests.py
@@ -1,27 +1,82 @@
 from django.test import TestCase
 from patients.models import Patient
+from django.test.utils import override_settings
+import haystack
+from django.core.management import call_command
+from haystack.query import SearchQuerySet
 
-class PatientTestCase_2(TestCase):
+#Define Test Index for ElasticSearch test cases
+TEST_INDEX = {
+	'default': {
+		'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+		'URL': 'http://127.0.0.1:9200/',
+		'TIMEOUT': 60 * 10,
+		'INDEX_NAME': 'test_index',
+	},
+}
+
+class CreatePatientDBEntryTestCase(TestCase):
 	def test_setUp(self):
-	        Patient.objects.create(first_name="Andrew", 
-        						last_name="Dudley", 
-        						dob="1989-02-22", 
-        						ssn="101-11-1010", 
-        						phone_number="623-910-0586",
-        						email = "addudley@asu.edu",
-        						address = "2027 E University Dr",
-        						city = "tempe",
-        						state = "Arizona",
-        						zip_code = 85281,
+			patient = Patient.objects.create(first_name="Andrew", 
+								last_name="Dudley", 
+								dob="1989-02-22", 
+								ssn="101-11-1010", 
+								phone_number="623-910-0586",
+								email = "addudley@asu.edu",
+								address = "2027 E University Dr",
+								city = "tempe",
+								state = "Arizona",
+								zip_code = 85281,
 
-        						health_insurance_provider="Aetna",
-        						health_insurance_id="XBH105 15014-40")
+								health_insurance_provider="Aetna",
+								health_insurance_id="XBH105 15014-40")
+			patient.save()
 
+class CreatePatientViewTestCase(TestCase):
+	def test_create_patient_view(self):
+		resp = self.client.get('/patient/add/')
+		self.assertEqual(resp.status_code, 200)
 
+class PatientProfileViewTestCase(TestCase):
+	fixtures = ['patients_testdata.json']
+	def test_patient_profile_view(self):
+		resp = self.client.get('/patient/1/')
+		self.assertEqual(resp.status_code, 200)
+		patient = resp.context['patient']
+		self.assertEqual(patient.id, 1)
 
-    # def test_animals_can_speak(self):
-    #     """Animals that can speak are correctly identified"""
-    #     lion = Animal.objects.get(name="lion")
-    #     cat = Animal.objects.get(name="cat")
-    #     self.assertEqual(lion.speak(), 'The lion says "roar"')
-    #     self.assertEqual(cat.speak(), 'The cat says "meow"')
+class PatientFullNameTestCase(TestCase):
+	fixtures = ['patients_testdata.json']
+	def test_patient_profile_view(self):
+		resp = self.client.get('/patient/1/')
+		self.assertEqual(resp.status_code, 200)
+		patient = resp.context['patient']
+		self.assertEqual(patient.get_full_name(), patient.last_name + ', ' + patient.first_name)
+
+class PatientUpdateCurrentHealthConditionsViewTestCase(TestCase):
+	fixtures = ['patients_testdata.json']
+	def test_current_health_condition(self):
+		resp = self.client.get('/patient/1/health/')
+		self.assertEqual(resp.status_code, 200)
+
+@override_settings(HAYSTACK_CONNECTIONS=TEST_INDEX)
+class PatientSearchViewTestCase(TestCase):
+	#fixtures = ['patients_testdata.json']
+	def setUp(self):
+		haystack.connections.reload('default')
+		super(PatientSearchViewTestCase, self).setUp()
+
+	def test_search_view(self):
+		resp = self.client.get('/search/')
+		self.assertEqual(resp.status_code, 200)
+
+	def test_search_query_view(self):
+		resp = self.client.get('/search/?q=sarah')
+		self.assertEqual(resp.status_code, 200)
+
+		# Test Search Query
+		self.sqs = SearchQuerySet()
+		self.assertEqual(self.sqs.auto_query('sarah').count(), 1)
+
+	def tearDown(self):
+		call_command('clear_index', interactive=False, verbosity=0)
